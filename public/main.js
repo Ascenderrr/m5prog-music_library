d8e1da84385b76195a8c7832feb192d1ad58d62b
user150434 <o.boudaati@gmail.com>
2025-09-19 14:45:03 +0200

    les 2 most stuff

---
+++ public/main.js
@@ -0,0 +1,8 @@
+const main = () => {
+    console.log("Hello, world!");
+    console.log("Script is successfully loaded and running!");
+};
+
+
+document.addEventListener('DOMContentLoaded', main);
+
